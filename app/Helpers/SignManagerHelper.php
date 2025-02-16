9fd3088bce7d572ceed2f1cecdd8053cd4f64a5e
warlyn <warlyn200@gmail.com>
2025-02-16 18:48:23 -0400

    Refactor SignManagerHelper and clean up comments
    Streamlined the namespace declaration and removed outdated or unnecessary comments to improve code clarity. This update focuses on maintaining clean and concise documentation while preserving the functionality.

--- app/Helpers/SignManagerHelper.php
+++ app/Helpers/SignManagerHelper.php
@@ -1,54 +1,15 @@
 <?php
 
-namespace app\Helpers;
+namespace App\Helpers;
 
 use Exception;
 use Selective\XmlDSig\Algorithm;
 use Selective\XmlDSig\CryptoSigner;
 use Selective\XmlDSig\PrivateKeyStore;
 
-/*
-Descargar la librería XMLDSIG desde https://github.com/selective-php/xmldsig
-Probado en las versiones de php 8.1.12 y 8.1.13
-*/
-
-/*
-Nota:
-
-**Refactorizaciones al archivo XmlSigner.php
-
-al instanciar la clase DOMDocument coloque la propiedad preserveWhiteSpace a false debido a que los espacios en blanco no deben ser preservados.
-
-Existe otra función que recibe un DOMDocument recuerde ajustar este valor antes de enviar el objeto.
-
-$xml->preserveWhiteSpace = true; cambiar a $xml->preserveWhiteSpace = false;
-
- **Por otro lado
-
-$canonicalData = $element->C14N(true, false); cambiar a $canonicalData = $element->C14N(false, false);
-
-en la linea 179,
-$c14nSignedInfo = $signedInfoElement->C14N(true, false); cambiar a $c14nSignedInfo = $signedInfoElement->C14N(false, false) o $signedInfoElement->C14N();
-
-puede dejarlos sin parámetros puesto que sus valores por defecto son false, es decir puede ser => $canonicalData = $element->C14N()
-
-**En la función appendSignature
-
-puede comentar las líneas 154 hasta la 170, los tag KeyValue, RSAKeyValue, Exponent no son necesarios
-
-**Recuerde habilitar la extensión openssl en su archivo php.ini, en algunas distribuciones esta deshabilitado por defecto.
-
-*/
-
 final class SignManagerHelper
 {
     /**
-     * The constructor.
-     *
-     * @param string $cert_store contenido del archivo p12
-     * @param string $password contraseña para acceder a la información contenida en el certificado
-     * @param string $xml contenido del archivo xml
-     *
      * @throws Exception
      */
     public function sign(string $cert_store, string $password, string $xml): string|Exception
