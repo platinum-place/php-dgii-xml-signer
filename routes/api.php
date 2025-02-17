61720454bde160204d34430b856aa020011784ac
warlyn <warlyn200@gmail.com>
2025-02-16 20:14:02 -0400

    Update default locale and modify API route method
    Changed the default application locale in `.env.example` from English (en) to Spanish (es) to better reflect the target audience. Also updated the API route for 'sing-manager' to use POST instead of GET to align with intended functionality.

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('sing-manager', \App\Http\Controllers\SignManagerController::class);
+Route::post('sing-manager', \App\Http\Controllers\SignManagerController::class);
