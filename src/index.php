5e8263cac2af6311caaa28d47e06e00ff834e289
warlyn <warlyn200@gmail.com>
2025-02-23 22:09:58 -0400

    Fix incorrect storage path in StorageService
    Adjusted the relative path in the `getFileContent` method to correctly locate files in the storage directory. This resolves potential file not found issues caused by the previous incorrect path.

--- src/index.php
+++ src/index.php
@@ -1,2 +1,17 @@
 <?php
-echo 'hello';
+
+require __DIR__ . '/../vendor/autoload.php';
+
+\Signer\Services\EnvironmentService::loadEnvironment();
+
+try {
+    $certContent = (new \Signer\Services\StorageService())->getFileContent('certs/' . getenv('CERT_NAME'));
+    $certPassword = getenv('CERT_PASSWORD');
+    $xmlContent = (new \Signer\Services\StorageService())->getFileContent('xml/' . getenv('XML_NAME'));
+
+    $signedXML = (new \Signer\Services\SignManagerService())->sign($certContent, $certPassword, $xmlContent);
+
+    echo $signedXML . PHP_EOL;
+} catch (Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
