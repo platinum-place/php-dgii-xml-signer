5d9b784755efc6d410c530fdf87558a0edc4862f
warlyn <warlyn200@gmail.com>
2025-02-23 22:27:31 -0400

    Add .env.example template for environment variables
    Provide a template for required environment variables, including placeholders for certificate details and XML name. This helps developers configure their local environments more effectively.

--- src/index.php
+++ src/index.php
@@ -5,9 +5,11 @@ require __DIR__ . '/../vendor/autoload.php';
 \Signer\Services\EnvironmentService::loadEnvironment();
 
 try {
-    $certContent = (new \Signer\Services\StorageService())->getFileContent('certs/' . getenv('CERT_NAME'));
+    $storageService = new \Signer\Services\StorageService();
+
+    $certContent = $storageService->getFileContent('certs/' . getenv('CERT_NAME'));
     $certPassword = getenv('CERT_PASSWORD');
-    $xmlContent = (new \Signer\Services\StorageService())->getFileContent('xml/' . getenv('XML_NAME'));
+    $xmlContent = $storageService->getFileContent('xml/' . getenv('XML_NAME'));
 
     $signedXML = (new \Signer\Services\SignManagerService())->sign($certContent, $certPassword, $xmlContent);
 
